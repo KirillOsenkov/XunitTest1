63dde9524491e605596789179ebd400a2d334eb8
Kirill Osenkov <github@osenkov.com>
2018-07-17 14:05:32 -0700

    Initial commit.

---
+++ XunitTest1/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace XunitTest1
+{
+    public class Class1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            Assert.True(false);
+        }
+
+        [Fact]
+        public void TestMethod2()
+        {
+        }
+    }
+}
